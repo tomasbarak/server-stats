8072228f0ff7a28a4b101a2d4972b433208c19e9
tomasbarak <barakcinthia@gmail.com>
2022-01-25 18:03:47 -0300

    Added network speed stats

--- app.js
+++ app.js
@@ -5,7 +5,30 @@ var cpu = osu.cpu;
 const express = require('express');
 const app = express();
 const { Server } = require("socket.io");
+const NetworkSpeed = require('network-speed');  // ES5
+const testNetworkSpeed = new NetworkSpeed();
 
+async function getNetworkDownloadSpeed() {
+  	const baseUrl = 'https://httpbin.org/stream-bytes/500000';
+  	const fileSizeInBytes = 500000;
+  	const speed = await testNetworkSpeed.checkDownloadSpeed(baseUrl, fileSizeInBytes);
+  	return speed;
+}
+
+async function getNetworkUploadSpeed() {
+	const options = {
+	  hostname: 'www.google.com',
+	  port: 80,
+	  path: '/catchers/544b09b4599c1d0200000289',
+	  method: 'POST',
+	  headers: {
+		'Content-Type': 'application/json',
+	  },
+	};
+	const fileSizeInBytes = 2000000
+	const speed = await testNetworkSpeed.checkUploadSpeed(options, fileSizeInBytes);
+	return speed;
+}
 const io = new Server(3000, { cors: {    origin: "*"  } });
 
 io.on("connection", (socket) => {
@@ -21,6 +44,11 @@ io.on("connection", (socket) => {
 
 function checkServerStats(){
 	disk.check('/', function(err, info){
+		Promise.all([getNetworkDownloadSpeed(), getNetworkUploadSpeed()]).then(function(values) {
+		const InternetSpeed = {
+			download: values[0],
+			upload: values[1],
+		}
 		cpu.usage().then(v => {
 			console.clear();
 
@@ -31,6 +59,9 @@ function checkServerStats(){
 			console.log('Free RAM:', bytesToSize(os.freemem()));
 
 			console.log('CPU Usage:', `${v}%`);
+			console.log('Download Speed:', `${InternetSpeed.download.mbps} Mbps`);
+			console.log('Upload Speed:', `${InternetSpeed.upload.mbps} Mbps`);
+			
 			io.emit('stats', {
 				free: bytesToSize(info.free),
 				total: bytesToSize(info.total),
@@ -44,6 +75,7 @@ function checkServerStats(){
 					percentageUsed: `${(((Math.abs(info.total - info.free)) / info.total) * 100).toFixed(2)}%`,
 					percentageFree: `${((info.free / info.total) * 100).toFixed(2)}%`
 				},
+				networkSpeed: InternetSpeed,
 				ram: {
 					total: os.totalmem(),
 					free: os.freemem(),
@@ -58,7 +90,10 @@ function checkServerStats(){
 				cpu: `${v}%`
 			});
 			setTimeout(checkServerStats, 1000);
-		})
+		});
+		}).catch(function(err) {
+			setTimeout(checkServerStats, 1000);
+		});
 	});
 }
 

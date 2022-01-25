246d7654c075279f4ce970e4b38a890bf7113d5b
tomasbarak <barakcinthia@gmail.com>
2022-01-25 16:09:34 -0300

    Added socketio support

--- app.js
+++ app.js
@@ -2,6 +2,22 @@ const disk = require('diskusage');
 const os = require('os');
 const osu = require('node-os-utils');
 var cpu = osu.cpu;
+
+const { Server } = require("socket.io");
+
+const io = new Server(3000, { cors: {    origin: "*"  } });
+
+io.on("connection", (socket) => {
+	console.log("New Connection");
+	socket.on("disconnect", () => {
+		console.log("Disconnect");
+	});
+	socket.on("freeRam", () => {
+		cleanRam();
+	})
+});
+
+
 function checkServerStats(){
 	disk.check('/', function(err, info){
 		cpu.usage().then(v => {
@@ -14,7 +30,33 @@ function checkServerStats(){
 			console.log('Free RAM:', bytesToSize(os.freemem()));
 
 			console.log('CPU Usage:', `${v}%`);
-			setTimeout(checkServerStats, 10);
+			io.emit('stats', {
+				free: bytesToSize(info.free),
+				total: bytesToSize(info.total),
+				disk: {
+					total: info.total,
+					free: info.free,
+					used: info.total - info.free,
+					formattedFree: bytesToSize(info.free),
+					formattedTotal: bytesToSize(info.total),
+					formattedUsed: bytesToSize(info.total - info.free),
+					percentageUsed: `${(((Math.abs(info.total - info.free)) / info.total) * 100).toFixed(2)}%`,
+					percentageFree: `${((info.free / info.total) * 100).toFixed(2)}%`
+				},
+				ram: {
+					total: os.totalmem(),
+					free: os.freemem(),
+					used: os.totalmem() - os.freemem(),
+					
+					formattedUsed: bytesToSize(os.totalmem() - os.freemem()),
+					percentageUsed: `${(((Math.abs(os.freemem() - os.totalmem())/os.totalmem())*100).toFixed(2))}%`,
+					percentageAvailable: `${((os.freemem()/os.totalmem())*100).toFixed(2)}%`,
+					formattedTotal: bytesToSize(os.totalmem()),
+					formattedFree: bytesToSize(os.freemem()),
+				},
+				cpu: `${v}%`
+			});
+			setTimeout(checkServerStats, 1000);
 		})
 	});
 }
@@ -30,4 +72,15 @@ function bytesToSize(bytes, decimals=2){
 	return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 };
 
+function cleanRam(){
+	const { exec } = require("child_process");
+	exec("sudo sync && echo 3 > /proc/sys/vm/drop_caches", (error, stdout, stderr) => {
+		if (error) {
+			return;
+		}
+		if (stderr) {
+			return;
+		}
+	});
+}
 checkServerStats();
